cf3adef2747dd51b61fd024af790f3b2b0a48491
Joel S. Roxell <joel.roxell@elva-group.com>
2023-04-23 17:23:13 +0200

    refactor(lib): move internal components to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,54 @@
+use account::error::ProvideErrorMetadata;
+use aws_sdk_account as account;
+use aws_sdk_iam as iam;
+use aws_sdk_sts as sts;
 use serde::{Deserialize, Serialize};
+
+pub async fn read_aws_information() -> Result<DisplayInformation, AwsError> {
+    let shared_config = aws_config::load_from_env().await;
+
+    let client_sts = sts::Client::new(&shared_config);
+    let client_account = account::Client::new(&shared_config);
+    let client_iam = iam::Client::new(&shared_config);
+
+    let (acc_res, sts_res, iam_res) = tokio::join!(
+        client_account.get_contact_information().send(),
+        client_sts.get_caller_identity().send(),
+        client_iam.list_account_aliases().send()
+    );
+
+    let alias = iam_res.map_err(|e| {
+        AwsError::IAMError(
+            e.message()
+                .unwrap_or("Failed to list account aliases")
+                .to_owned(),
+        )
+    })?;
+    let sts_res = sts_res
+        .map_err(|e| AwsError::STSError(e.message().unwrap_or("STS request failed").to_owned()))?;
+    let contact_info = acc_res.map_err(|e| {
+        AwsError::AccoutError(
+            e.message()
+                .unwrap_or("Failed to read account information from AWS")
+                .to_owned(),
+        )
+    })?;
+
+    Ok(DisplayInformation {
+        name: match contact_info.contact_information() {
+            Some(contact) => contact.full_name().unwrap_or(""),
+            None => "",
+        }
+        .to_string(),
+        arn: sts_res.arn().to_owned().unwrap_or("").to_string(),
+        user_id: sts_res.user_id().unwrap_or("").to_string(),
+        account: sts_res.account().unwrap_or("").to_string(),
+        aliases: match alias.account_aliases() {
+            Some(a) => a.to_owned(),
+            None => vec![],
+        },
+    })
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DisplayInformation {
@@ -7,4 +57,11 @@ pub struct DisplayInformation {
     pub user_id: String,
     pub arn: String,
     pub aliases: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum AwsError {
+    IAMError(String),
+    STSError(String),
+    AccoutError(String),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,12 @@
-use aws_sdk_account as account;
-use aws_sdk_iam as iam;
-use aws_sdk_sts as sts;
-use std::error::Error;
-
-use whoiam::DisplayInformation;
+use whoiam::{read_aws_information, AwsError};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let shared_config = aws_config::load_from_env().await;
-
-    let client_sts = sts::Client::new(&shared_config);
-    let client_iam = iam::Client::new(&shared_config);
-    let client_account = account::Client::new(&shared_config);
-
-    let (acc_res, sts_res, ali_res) = tokio::join!(
-        client_account.get_contact_information().send(),
-        client_sts.get_caller_identity().send(),
-        client_iam.list_account_aliases().send()
-    );
-
-    let alias = ali_res.expect("Failed to read alias from AWS");
-    let sts_res = sts_res.expect("STS request failed");
-    let contact_info = acc_res.expect("Failed to read account information from AWS");
-    let contact_info = contact_info
-        .contact_information()
-        .expect("Contact information is missing from the response");
+async fn main() -> Result<(), AwsError> {
+    let info = read_aws_information().await?;
 
     println!(
         "{}",
-        serde_json::to_string_pretty(&DisplayInformation {
-            name: contact_info.full_name().unwrap_or("").to_string(),
-            arn: sts_res.arn().unwrap_or("").to_string(),
-            user_id: sts_res.user_id().unwrap_or("").to_string(),
-            account: sts_res.account().unwrap_or("").to_string(),
-            aliases: match alias.account_aliases() {
-                Some(a) => a.to_owned(),
-                None => vec![],
-            }
-        })
-        .expect("Failed to stringify user data")
+        serde_json::to_string_pretty(&info).expect("Failed to stringify user data")
     );
 
     Ok(())
